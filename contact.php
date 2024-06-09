6d39dba9267abfdc4945359cd0f3d4ff45e8d0df
saifull03 <115652846+saifull03@users.noreply.github.com>
2024-06-09 16:26:05 +0600

    Add files via upload
    initial of personal website

---
+++ contact.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Contact Saiful</h1>
+    <table cellspacing="20">
+        <tr>
+            <th>Email</th>
+            <td>soton.me@gmail.com</td>
+        </tr>
+        <tr>
+            <th>Phone</th>
+            <td>01778384700</td>
+        </tr>
+
+    </table>
+    <hr>
+
+    <form action="index.html" method="post">
+
+        <label>Your Name:</label>
+        <br>
+        <input type="text">
+        <br>
+        <label for="">Number</label>
+        <br>
+        <input type="number">
+        <br>
+        <label for="">Email</label>
+        <br>
+        <input type="email">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Saiful Personal Site</title>
+</head>
+<body>
+
+<table cellspacing = "20">
+    <tr>
+        <td>
+
+        <img src="saiful.png" alt="Saiful Profile Picture" width="200" height="200">
+        <h1>Saiful Islam</h1>
+        </td>
+
+        <td>
+        <h3>Biography</h3>
+<p>
+    Md Saiful Islam born in 2003 ,  Jessore Dhaka. He completed his SSC from KAAI and HSC from GRC.
+    He achieved GPA 5 both .
+    Now he's completing his BSc in CSE from IUBAT.
+</p>
+
+<hr>
+        </td>
+    </tr>
+</table>
+
+
+    <hr>
+ 
+<h3>Skills</h3>
+<table cellspacing= "20" >
+    <thead>
+        <th> Work</th>
+        <th>Experience</th>
+    </thead>
+    <tr>
+        <td>HTML</td>
+        <td>⭐⭐⭐⭐⭐</td>
+    </tr>
+    <tr>
+        <td>CSS</td>
+        <td>⭐⭐⭐</td>
+    </tr>
+    <tr>
+        <td>Javascript</td>
+        <td>⭐</td>
+    </tr>
+</table>
+
+
+<a href="contact.php">Contact Us</a>
+
+</body>
+</html>
